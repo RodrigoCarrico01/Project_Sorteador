cf06ad6069169f7531bc69ce1a198ada97e2c7f8
Rodrigo Martins <mr.rodrigocarrico@gmail.com>
2025-04-02 10:11:23 +0100

    receiving values

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 // Captura dos elementos
+const form = document.querySelector("form")
 const numbers = document.getElementsByClassName("number-input")
-
+const quantityInput = document.getElementById("quantity")
+const firstNumberInput = document.getElementById("first-number")
+const lastNumberInput = document.getElementById("last-number")
+const noRepeatCheckbox = document.getElementById("no-repeat")
 
 
 // Não permitir letras
@@ -11,5 +15,15 @@ for (let i = 0; i < numbers.length; i++) {
     numbers[i].value = value
 
   }
+}
+
+form.onsubmit = (error) => {
+  error.preventDefault(); 
+  const quantity = parseInt(quantityInput.value, 10)
+  const first = parseInt(firstNumberInput.value, 10)
+  const last = parseInt(lastNumberInput.value, 10)
+  const noRepeat = noRepeatCheckbox.checked
+
+
 }
 
